9f43847ffb0453bbc0557ffe8e1bfecb817338d9
Kenny Onishi <kenny619@gmail.com>
2024-08-26 12:14:13 +0900

    v1.0.0

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,4 @@ export default defineConfig({
 	},
 	files: ["./tests/*.test.ts"], // Adjust the pattern to match your test files
 	extensions: ["ts"],
-	require: ["ts-node/register"], // Use ts-node to execute TypeScript files directly
 });
